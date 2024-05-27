a5901436731b7ed6810da2d4fc6f0face107c86f
lemonacy <lemonacy@163.com>
2024-05-27 15:12:14 +0800

    补充了一下系统调用函数的定义 - sys_call_table[]

--- arch/x86/entry/syscall_64.c
+++ arch/x86/entry/syscall_64.c
@@ -9,10 +9,10 @@
 
 #define __SYSCALL(nr, sym) extern long __x64_##sym(const struct pt_regs *);
 #include <asm/syscalls_64.h>    // 这个文件是编译时生成的arch/x86/include/generated/asm/syscalls_64.h，内容为__SYSCALL(0, sys_read)等
-#undef __SYSCALL
+#undef __SYSCALL                // 清楚第10行定义的__SYSCALL宏，下面14行要重新定义其作用
 
-#define __SYSCALL(nr, sym) __x64_##sym,
+#define __SYSCALL(nr, sym) __x64_##sym, // 重新定义__SYSCALL宏，给下面数组中包含函数用
 
 asmlinkage const sys_call_ptr_t sys_call_table[] = {
-#include <asm/syscalls_64.h>
+#include <asm/syscalls_64.h>    // 上面是通过extern的方式定义分散在各个模块的系统调用函数，此处就把上面声明的那些函数定义到sys_call_table数组中来
 };
